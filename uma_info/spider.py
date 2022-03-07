ac265ba8591787fbf07b856d2021f3ca868d4d25
穿越电线 <71607036+CYDXDianXian@users.noreply.github.com>
2022-03-07 21:49:06 +0800

    去除多余的代码

--- uma_info/spider.py
+++ uma_info/spider.py
@@ -1,4 +1,3 @@
-import httpx
 import json
 import time
 import os
@@ -14,7 +13,7 @@ import demjson
 # 是否使用ocr_space接口，默认启用
 ENABLE_OCR_SPACE = True
 # ocr_space接口的apikey
-APIKEY = ''
+APIKEY = 'K89720689788957'
 
 async def uma_spider():
     current_dir = os.path.join(os.path.dirname(__file__), 'config.json')
