51180a6b7de3c8aadf2dfcb6418cec30a56d8cf6
azmiao <2362020227@qq.com>
2022-03-19 10:32:01 +0800

    给有时候发不出节奏榜加一句报错吧2333

--- uma_support_chart/__init__.py
+++ uma_support_chart/__init__.py
@@ -2,6 +2,7 @@ from hoshino import Service
 from hoshino.typing import MessageSegment
 from hoshino.util import pic2b64
 from .get_url import generate_img
+import httpx
 
 sv_help = '''
 [速卡节奏榜] 对应速度卡
@@ -23,7 +24,11 @@ async def SSR_speed_chart(bot, ev):
     sup_type = ev['match'].group(1)
     if sup_type not in ['速卡', '耐卡', '力卡', '根卡', '根卡', '智卡']:
         return
-    end_img_tmp, update_info = await generate_img(sup_type)
+    try:
+        end_img_tmp, update_info = await generate_img(sup_type)
+    except httpx.ConnectError:
+        msg = 'biwiki连接失败，请重新输入命令重试！'
+        await bot.finish(ev, msg)
     if not end_img_tmp:
         msg = f'新版{sup_type}节奏榜还未出炉，本地也未检测到旧版{sup_type}节奏榜。请等待新版节奏榜更新'
         await bot.finish(ev, msg)
