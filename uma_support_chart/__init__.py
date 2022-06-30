e82dc03bbc3206c48ec670698d60f31926f64616
azmiao <2362020227@qq.com>
2022-06-30 10:55:23 +0800

    v2.1.0  新增【台服】支援卡节奏榜功能

--- uma_support_chart/__init__.py
+++ uma_support_chart/__init__.py
@@ -1,4 +1,5 @@
 from .get_url import generate_img
+from .get_url_tw import generate_img_tw
 from hoshino import Service
 
 sv_help = '''
@@ -8,6 +9,7 @@ sv_help = '''
 [根卡节奏榜] 对应根性卡
 [智卡节奏榜] 对应智力卡
 [友人卡节奏榜] 对应友人卡和团队卡
+※若需要查繁中服节奏榜，请在命令前面加“台服”，例如：台服速卡节奏榜
 '''.strip()
 
 sv = Service('uma_support_chart', help_=sv_help)
@@ -17,10 +19,13 @@ sv = Service('uma_support_chart', help_=sv_help)
 async def help(bot, ev):
     await bot.send(ev, sv_help)
 
-@sv.on_rex(r'^(\S{1,2})卡节奏榜$')
+@sv.on_rex(r'^(台服)?(\S{1,2})卡节奏榜$')
 async def SSR_speed_chart(bot, ev):
-    sup_type = ev['match'].group(1)
+    sup_type = ev['match'].group(2)
     if sup_type not in ['速', '耐', '力', '根', '智', '友人']:
         return
-    msg = await generate_img(sup_type)
+    if not ev['match'].group(1):
+        msg = await generate_img(sup_type)
+    else:
+        msg = await generate_img_tw(sup_type)
     await bot.send(ev, msg)
--- uma_support_chart/update_init.py
+++ uma_support_chart/update_init.py
@@ -4,9 +4,15 @@ import json
 from hoshino import logger
 
 current_dir = os.path.join(os.path.dirname(__file__), 'sup_config.json')
+current_dir_tw = os.path.join(os.path.dirname(__file__), 'sup_config_tw.json')
 async def update():
     if not os.path.exists(current_dir):
         img_dict = {}
         with open(current_dir, 'w', encoding='UTF-8') as f:
             json.dump(img_dict, f, indent=4, ensure_ascii=False)
         logger.info(f'====节奏榜配置文件不存在，现已成功创建====')
+    if not os.path.exists(current_dir_tw):
+        img_dict = {}
+        with open(current_dir_tw, 'w', encoding='UTF-8') as f:
+            json.dump(img_dict, f, indent=4, ensure_ascii=False)
+        logger.info(f'====台服节奏榜配置文件不存在，现已成功创建====')
