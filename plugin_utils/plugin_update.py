c5767b9d53f517e90b56bf5785e01157b05f99ec
azmiao <2362020227@qq.com>
2024-01-18 10:23:00 +0800

    fix plugin_update function

--- plugin_utils/plugin_update.py
+++ plugin_utils/plugin_update.py
@@ -1,28 +1,30 @@
-import re
-import os
 import json
+import os
+import re
+from datetime import datetime, timedelta
+
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 from git.repo import Repo
+from hoshino import logger, config, get_bot
 
 from ..plugin_utils.base_util import get_proxy, get_update_type
-from hoshino import logger, config, get_bot
 
 version_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'version.json')
 url = 'https://github.com/azmiao/uma_plugin'
+
 
 # 初始化马娘插件版本
 async def init_plugin():
     update_type = await get_update_type()
     if os.path.exists(version_path):
-            return
+        return
     if update_type == 'no':
         logger.info('【马娘插件】正在从本地获取马娘插件版本...')
         with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'README.md'), 'r', encoding='utf-8') as f:
             text = f.read()
         version = re.search(r'release-([0-9]+\.[0-9]+\.[0-9]+f?)-orange\.svg', text).group(1)
-        commit_time = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
+        commit_time = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
     else:
         logger.info('【马娘插件】正在在线获取马娘插件版本...')
         data_list, version = await get_commits(url)
@@ -35,12 +37,15 @@ async def init_plugin():
         json.dump(version_data, f, indent=4, ensure_ascii=False)
     logger.info('【马娘插件】马娘插件版本获取成功')
 
+
 # 更新马娘插件
 async def plugin_update_auto():
     update_type = await get_update_type()
-    if update_type == 'no': return
+    if update_type == 'no':
+        return
     flag, is_sup, msg = await judge_update(url)
-    if not flag: return
+    if not flag:
+        return
     # 如果不是强制更新的版本
     if not is_sup:
         git_url = f'https://ghproxy.com/{url}'
@@ -53,22 +58,24 @@ async def plugin_update_auto():
             repo.remote('origin').set_url(git_url)
         repo.git.pull()
         logger.info('【马娘插件】已更新至最新！')
-    superid = config.SUPERUSERS[0]
+    super_id = config.SUPERUSERS[0]
     bot = get_bot()
-    await bot.send_private_msg(user_id=superid, message=msg)
+    await bot.send_private_msg(user_id=super_id, message=msg)
+
 
 # 检测马娘插件是否有更新
-async def judge_update(url):
+async def judge_update(rep_url):
     with open(version_path, 'r', encoding="UTF-8") as f:
         version_data = json.load(f)
     old_time = datetime.strptime(version_data['commit_time'], "%Y-%m-%d %H:%M:%S")
     old_version = version_data['version']
-    data_list, version = await get_commits(url)
+    data_list, version = await get_commits(rep_url)
     new_time = data_list[0]['time']
     if new_time <= old_time:
         logger.info('【马娘插件】未检测到更新！')
         return False, False, ''
-    msg_header = '【该版本为强制更新版，将不会自动更新，请手动使用git pull -f更新】\n' if version.endswith('f') and old_version != version else '已成功自动更新，请手动重启bot！\n'
+    msg_header = '【该版本为强制更新版，将不会自动更新，请手动使用git pull -f更新】\n' if version.endswith(
+        'f') and old_version != version else '已成功自动更新，请手动重启bot！\n'
     msg = f'【马娘插件】\n版本：{old_version} -> {version}\n{msg_header}更新细节：'
     for data in data_list:
         data_time = data['time']
@@ -81,35 +88,34 @@ async def judge_update(url):
         json.dump(version_data, f, indent=4, ensure_ascii=False)
     return True, version.endswith('f') and old_version != version, msg
 
+
 # 调整太平洋时间
 async def change_time(raw_time):
     raw_time = str(raw_time).replace('Z', '')
-    txtfmt = raw_time[:10]+ " " + raw_time[11:19]
-    dt = datetime.strptime(txtfmt,"%Y-%m-%d %H:%M:%S")
+    txt_fmt = raw_time[:10] + " " + raw_time[11:19]
+    dt = datetime.strptime(txt_fmt, "%Y-%m-%d %H:%M:%S")
     cur_time = dt + timedelta(hours=8)
     return cur_time
 
+
 # 获取commits的信息和版本号 | 最近10条
-async def get_commits(url):
+async def get_commits(rep_url):
     data_list = []
     # version
-    res = requests.get(url, proxies = get_proxy(), timeout=20)
+    res = requests.get(rep_url, proxies=get_proxy(), timeout=20)
     version = re.search(r'release-([0-9]+\.[0-9]+\.[0-9]+f?)-orange\.svg', res.text)
     # commit
-    resp = requests.get(url+"/commits", proxies = get_proxy(), timeout=20)
+    resp = requests.get(rep_url + "/commits", proxies=get_proxy(), timeout=20)
     soup = BeautifulSoup(resp.text, 'lxml')
-    block_list = soup.find_all('li', {"class": re.compile("Box-row Box-row--focus-gray[\s\S]*?")})
-    for block in block_list[:10]:
-        if block.find('span', {"class": "hidden-text-expander inline"}):
-            block.find('span', {"class": "hidden-text-expander inline"}).decompose()
-        try:
-            author = block.find('a', {"class": "commit-author user-mention"}).text
-        except:
-            author = block.find('span', {"class": "commit-author user-mention"}).text
-        cur_time = await change_time(block.find('relative-time').get('datetime'))
-        data_list.append({
-            'title': block.find('p', {"class": "mb-1"}).text.strip(),
-            'author': author,
-            'time': cur_time
-        })
+    soup_find = soup.find('script', {'data-target': 'react-app.embeddedData'})
+    raw_info = json.loads(soup_find.text)
+    commit_info_list = raw_info['payload']['commitGroups']
+    for commit_info_by_date in commit_info_list:
+        for commit_info in commit_info_by_date['commits']:
+            commit_message = commit_info['shortMessage']
+            authors_name = ','.join([info['displayName'] for info in commit_info['authors']])
+            time = await change_time(commit_info['committedDate'])
+            data_list.append({'title': commit_message, 'author': authors_name, 'time': time})
+            if len(data_list) >= 10:  # 如果要只取最近10次commit
+                break
     return data_list, version.group(1)
