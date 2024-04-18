115b8fff21229e448b5cc55d39e9913fd5ea25d7
azmiao <2362020227@qq.com>
2024-04-18 10:40:11 +0800

    修复相性名称对不上的问题

--- uma_compatibility/update_type.py
+++ uma_compatibility/update_type.py
@@ -9,6 +9,8 @@ async def update():
     res = httpx.get(url, timeout=15)
     soup = BeautifulSoup(res.text, 'lxml')
     type_text = soup.find('div', {'id': 'relation'}).text
+    type_text = type_text.replace('圣王光环', '帝王光辉')
+    type_text = type_text.replace('东海帝王', '东海帝皇')
     current_dir = os.path.join(os.path.dirname(__file__), 'relation_type.json')
     with open(current_dir, 'w', encoding='UTF-8') as f:
         f.write(type_text.strip())
